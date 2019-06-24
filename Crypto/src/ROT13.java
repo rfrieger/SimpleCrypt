41f2f47ebdd0c7128ef8202b0a01ae7151d6a4a0
rfrieger <rfrieger@gmail.com>
2019-06-24 18:45:18 -0400

    first part done

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,32 +1,61 @@
+import java.nio.charset.CharacterCodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
+    Character cs;
+    Character cf;
+    Integer amountToRotate;
 
     ROT13(Character cs, Character cf) {
+        this.cs = cs;
+        this.cf = cf;
+        this.amountToRotate = cf.charValue() - cs.charValue();
     }
 
     ROT13() {
+        amountToRotate = 13;
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        return encrypt(text);
     }
 
     public String encrypt(String text) {
-        return text;
+        String returnString = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'm') c += amountToRotate;
+            else if (c >= 'A' && c <= 'M') c += amountToRotate;
+            else if (c >= 'n' && c <= 'z') c -= amountToRotate;
+            else if (c >= 'N' && c <= 'Z') c -= amountToRotate;
+            returnString += c;
+        }
+        return returnString;
     }
 
     public String decrypt(String text) {
-        return text;
+        return encrypt(text);
+
     }
 
-    public static String rotate(String s, Character c) {
+    public String rotate(String s, Character c) {
+        Character toThis = Character.toLowerCase(c);
+        Character a = 'a';
+        Integer toMove =  toThis.charValue() - a.charValue();
 
-        return "";
+        final int length = s.length();
+        if( length == 0 ) return "";
+        final int offset = ((toMove % length) + length) % length;
+        return s.substring( offset, length ) + s.substring( 0, offset );
     }
+
 
 }
--- Crypto/test/ROT13Test.java
+++ Crypto/test/ROT13Test.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class ROT13Test {
+public class    ROT13Test {
 
 
     @Test
